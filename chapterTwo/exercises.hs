995465e736b705135df5e5939be3631ea2c2df46
Wyatt Barnes <wyattbarnes@protonmail.com>
2018-06-29 20:47:07 -0500

    Refactor to one liner

--- chapterTwo/exercises.hs
+++ chapterTwo/exercises.hs
@@ -17,10 +17,6 @@ getLastV2 xs = head (reverse xs)
 getLastV3 xs = head (drop (length xs -1) xs)
 
 -- Exercise Five
-removeLast xs = reverse (drop 1 a)
-                        where
-                            a = reverse xs
+removeLast xs = reverse (drop 1 reverse xs)
 
-removeLastV2 xs = reverse(tail a)
-                            where
-                                a = reverse xs
+removeLastV2 xs = reverse(tail (reverse xs))
